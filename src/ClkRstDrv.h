0bebf5742f65db329b16ad5937efa389b3242f3e
Antti Nurmi <antti.nurmi@tuni.fi>
2025-01-04 23:17:36 +0200

    Initial commit

---
+++ src/ClkRstDrv.h
@@ -0,0 +1,34 @@
+class ClkRstDrv {
+    private:
+        SimCtx* cx;
+        vluint64_t rst_counter;
+        vluint64_t clk_counter;
+        uint32_t   per_counter;
+
+    public:
+        ClkRstDrv(SimCtx* cx){
+            this->cx = cx;
+            this->rst_counter = 0;
+            this->clk_counter = 0;
+            this->per_counter = 0;
+        }
+
+        void reset(vluint64_t rst_delay){
+            if(rst_counter == rst_delay)
+                cx->dut->rst_ni = 1;
+            else
+                rst_counter++;
+        }
+
+        void clock(vluint64_t clk_delay, uint32_t clk_per) {
+            if (clk_counter == clk_delay){
+                if (per_counter == clk_per - 1) {
+                    per_counter = 0;
+                    cx->dut->clk_i ^= 1;
+                } else {
+                    per_counter++;
+                }
+            } else
+                clk_counter++;
+        }
+};
